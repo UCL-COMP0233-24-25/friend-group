92e66d26174ebfa6a1cfa4ae818ff18bafe40fe4
wangxinping <zczqx99@ucl.ac.uk>
2024-10-14 10:21:38 +0100

    add personal information

--- group.py
+++ group.py
@@ -2,4 +2,14 @@
 
 # Your code to go here...
 
-my_group =
+my_group =[{ "name": "Luhan", "age": 22, "job": "Student"},   
+        {  "name": "Wangjiayi", "age": 22, "job": "Student"},
+         {"name": "Wangxinping", "age": 22, "job": "Student"}]  
+
+
+
+
+for row in my_group:  
+    print(f"{row['name']} is {row['age']}, a {row['job']}.")  
+
+
