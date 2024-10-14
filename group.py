324b923ff60fff65292d6ce92066f681a82a1628
Zhexuan <ucapzso@ucl.ac.uk>
2024-10-14 22:50:21 +0800

    Complete Stretch Goal

--- group.py
+++ group.py
@@ -8,30 +8,58 @@ my_group = {
     "Nash":{"age":34,"job":"chef","relationship":{"landlord":["Zalika"]}},
 }
 
-def forget(person1,person2):
+def forget(person1, person2):
+    
     if person1 not in my_group:
         print(f"{person1} is not in the group.")
         return
     if person2 not in my_group:
         print(f"{person2} is not in the group.")
         return
-    to_remove_1 = []
-    to_remove_2 = []
-    for relationship, names in my_group[person1]["relationship"].items():
-        if person2 in names:
-            my_group[person1]["relationship"][relationship].remove(person2)
-            if my_group[person1]["relationship"][relationship] == []:
-                to_remove_1.append(relationship)
-    for relationship, names in my_group[person2]["relationship"].items():
-        if person1 in names:
-            my_group[person2]["relationship"][relationship].remove(person1)
-            if my_group[person2]["relationship"][relationship] == []:
-                to_remove_2.append(relationship)
 
-    for key in to_remove_1:
-        del(my_group[person1]['relationship'][key])
-    
-    for key in to_remove_2:
-        del(my_group[person2]['relationship'][key])
-    
+    def remove_relationship(person_a, person_b):
+        to_remove = []
+        for relationship, names in my_group[person_a]["relationship"].items():
+            if person_b in names:
+                my_group[person_a]["relationship"][relationship].remove(person_b)
+                if not my_group[person_a]["relationship"][relationship]:
+                    to_remove.append(relationship)
+        for relationship in to_remove:
+            del my_group[person_a]["relationship"][relationship]
+
+    remove_relationship(person1, person2)
+    remove_relationship(person2, person1)
+
     return
+
+def add_person(name,age,job,relation):
+    
+    if name in my_group:
+        print(f"{name} is already in my_group.")
+        return
+     
+    my_group[name] = {'age':age,'job':job,'relationship':relation}
+
+    for relationship, people in relation.items():
+        for person in people:
+            if person not in my_group:
+                my_group[person] = {'relationship':{relationship:[name]}}
+            else:
+                if relationship in my_group[person]['relationship']:
+                    my_group[person]['relationship'][relationship].extend([name])    
+                else:
+                    my_group[person]['relationship'][relationship] = [name]
+
+    return
+
+def average_age(group):
+    
+    import numpy as np
+    
+    n = len(my_group.keys())
+    ages = np.zeros(n)
+    for i, (person, info) in enumerate(my_group.items()):
+        ages[i] = info['age']
+    average_age = np.average(ages)
+
+    return average_age
