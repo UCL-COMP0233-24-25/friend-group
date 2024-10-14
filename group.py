d3bacf0a31fb558b542f7e6a62e04f81644d13aa
DanielChau128 <ccaedrc@ucl.ac.uk>
2024-10-14 10:47:43 +0100

    WIP data structure for Daniel and Shyam

--- group.py
+++ group.py
@@ -2,4 +2,17 @@
 
 # Your code to go here...
 
-my_group =
+my_group = Daniel, Shyam
+
+class persons:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.relation = {}
+
+    def add_relation(self, add_name, add_relation):
+        self.relation[add_name] = add_relation
+
+Daniel = persons{name=Daniel, age=23}
+
+Shyam = persons{name=Shyam, age=}        
