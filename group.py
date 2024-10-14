cccb8bc3ed82dcd9b1ed59f4872c0db1ae7c47e4
AngelosZou <yutong.zou.24@ucl.ac.uk>
2024-10-14 11:42:31 +0100

    Add some summaries through comprehension expressions

--- group.py
+++ group.py
@@ -1,9 +1,28 @@
 """An example of how to represent a group of acquaintances in Python."""
+import numpy as np
 
 #For the relationship of each person, it is like: the front person is the “relationship” of the behind person. 
 my_group = {
-    "Jill":{"age":26,"job":"biologist","realtionship":{"friend":["Zalika"],"partner":["John"]}},
-    "Zalika":{"age":28,"job":"artist","realtionship":{"friend":["Jill"]}},
-    "John":{"age":27,"job":"writer","realtionship":{"partner":["Jill"]}},
-    "Nash":{"age":34,"job":"chef","realtionship":{"landlord":["Zalika"]}},
+    "Jill":{"age":26,"job":"biologist","relationship":{"friend":["Zalika"],"partner":["John"]}},
+    "Zalika":{"age":28,"job":"artist","relationship":{"friend":["Jill"]}},
+    "John":{"age":27,"job":"writer","relationship":{"partner":["Jill"]}},
+    "Nash":{"age":34,"job":"chef","relationship":{"landlord":["Zalika"]}},
 }
+
+
+# Max Age
+print("Max age:", end=" ")
+print(np.max([my_group[person]["age"] for person in my_group]))
+
+# Average Relation
+print("Average relationship", end=" ")
+print(np.mean([len(my_group[person]["relationship"]) for person in my_group]))
+
+# Max age with at least one relation
+print("Max age with at least one relation:", end=" ")
+print(np.max([my_group[person]["age"] for person in my_group if len([my_group[person]["relationship"]])>=1]))
+
+# the maximum age of people in the group that have at least one friend
+print("Max age with at least one friend:", end=" ")
+print(np.max([my_group[person]["age"] for person in my_group if 'friend' in my_group[person]["relationship"]]))
+
