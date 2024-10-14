583bbb8d0f1b28eb53c2d0ef62a1cb7ff6e558a5
Wei Hong <weihong.chua19@sps.nus.edu.sg>
2024-10-14 10:33:27 +0100

    Appended code describing characters and their relationships

--- group.py
+++ group.py
@@ -1,5 +1,19 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
+week3_group=[{
+"name":"Jill","age":26,"job":"biologist"   
+,"connections":[{"name":"Zalika","relationship":"friend"},{"name":"John","relationship":"partner"}]},
 
-my_group =
+{
+"name":"Zalika","age":28,"job":"artist"   
+,"connections":[{"name":"Jill","relationship":"friend"}]},
+
+{
+"name":"John","age":27,"job":"writer"   
+,"connections":[{"name":"Jill","relationship":"partner"}]},
+
+{"name":"Nash","age":34,"job":"chef"   
+,"connections":[{"name":"John","relationship":"cousin"},{"name":"Zalika","relationship":"landlord"}]}
+]
+# my_group =
