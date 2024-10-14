a0b9407af343bce090279ac0573b8234427526bb
Zhexuan <ucapzso@ucl.ac.uk>
2024-10-14 17:31:31 +0800

    Initialize group data structure

--- group.py
+++ group.py
@@ -1,5 +1,9 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
-
-my_group =
+#For the relationship of each person, it is like: the front person is the “relationship” of the behind person. 
+my_group = {
+    "Jill":{"age":26,"job":"biologist","realtionship":{"friend":["Zalika"],"partner":["John"]}},
+    "Zalika":{"age":28,"job":"artist","realtionship":{"friend":["Jill"]}},
+    "Zalika":{"age":27,"job":"writer","realtionship":{"partner":["Jill"]}},
+    "Nash":{"age":34,"job":"chef","realtionship":{"landlord":["Zalika"]}},
+}
