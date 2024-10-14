8804a664f67c5d6604ebdaf73e3525374c8c22b3
Zhexuan <ucapzso@ucl.ac.uk>
2024-10-14 22:54:47 +0800

    Fix a mistake

--- group.py
+++ group.py
@@ -2,8 +2,8 @@
 
 #For the relationship of each person, it is like: the front person is the “relationship” of the behind person. 
 my_group = {
-    "Jill":{"age":26,"job":"biologist","realtionship":{"friend":["Zalika"],"partner":["John"]}},
-    "Zalika":{"age":28,"job":"artist","realtionship":{"friend":["Jill"]}},
-    "John":{"age":27,"job":"writer","realtionship":{"partner":["Jill"]}},
-    "Nash":{"age":34,"job":"chef","realtionship":{"landlord":["Zalika"]}},
+    "Jill":{"age":26,"job":"biologist","relationship":{"friend":["Zalika"],"partner":["John"]}},
+    "Zalika":{"age":28,"job":"artist","relationship":{"friend":["Jill"]}},
+    "John":{"age":27,"job":"writer","relationship":{"partner":["Jill"]}},
+    "Nash":{"age":34,"job":"chef","relationship":{"landlord":["Zalika"]}},
 }
