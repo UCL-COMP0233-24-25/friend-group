40697ace04360512b027fb59f310fee7b9a2adb4
Jennivine <45113203+Jennivine@users.noreply.github.com>
2024-10-14 21:42:10 +0100

    implemented a data structure to represent a group of acquaintances using dictionaries and lists

--- group.py
+++ group.py
@@ -2,4 +2,33 @@
 
 # Your code to go here...
 
-my_group =
+my_group = [
+    {
+        "Name": "Jill",
+        "Age": 26,
+        "Job": "Biologist",
+        "Connections": {"Friends": ["Zalika"], 
+                        "Partner": "John"}
+    },
+    {
+        "Name": "Zalika",
+        "Age": 28,
+        "Job": "Artist",
+        "Connections": {"Friends": ["Jill"], 
+                        "Landlord": "Nash"}
+    },
+    {
+        "Name": "John",
+        "Age": 27,
+        "Job": "Writer",
+        "Connections": {"Partner": "Jill"}
+    },
+    {
+        "Name": "Nash",
+        "Age": 34,
+        "Job": "Chef",
+        "Connections": {"Cousins": ["John"], 
+                        "Tennants": ["Zalika"]}
+    }
+]
+
