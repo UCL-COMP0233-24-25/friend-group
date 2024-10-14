9c0f965a6f2a1a35cbeade483b8468978e1305a6
Charlotte Savage <charlotte.e.savage@gmail.com>
2024-10-14 10:57:27 +0100

    added  nested structure

--- group.py
+++ group.py
@@ -1,37 +1,45 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
-
-my_group = [
+my_group = {
     {
         "name": "Jill",
         "job": "biologist",
         "age": 26,
+        "relations": {
         "friends": {"Zalika"},
         "partner": "John",
+        },
     },
     {
         "name": "Zalika",
         "job": "artist",
         "age": 28,
+        "relations":{
         "friends": {"Jill"},
         "landlord": "Nash",
         "partner": None,
+        }
     },
     {
         "name": "John",
         "job": "writer",
         "age": 27,
-        "friends": {},
+        "relations":
+        {
+        "friends": set(),
         "partner": "Jill",
+        },
     },
     {
         "name": "Nash",
         "job": "chef",
         "age": 34,
-        "friends": {},
+        "relations":
+        {
+        "friends": set(),
         "cousin": {"John"},
         "tenant": "Zalika",
         "partner": "Jill",
+        },
     },
-]
+}
