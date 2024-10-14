da010af1a2fd5ae2d85590c1bffc9e871ae7fad1
Chayapon Thunsetkul <chayapon.thunsetkul.24@ucl.ac.uk>
2024-10-14 11:33:49 +0100

    implemented functions using comprehension to print out various values

--- group.py
+++ group.py
@@ -45,3 +45,35 @@ nash = {
 }
 
 my_group = [jill, zalinka, john, nash]
+
+def max_age(group):
+    """Returns the maximum age of people in the group."""
+    return max(person['age'] for person in group)
+
+def average_relations(group):
+    """Returns the average number of connections (relations) among members of the group."""
+    total_connections = sum(len(person['connections']) for person in group)
+    return total_connections / len(group)
+
+def max_age_with_relations(group):
+    """Returns the maximum age of people in the group that have at least one connection."""
+    people_with_relations = [person['age'] for person in group if person['connections']]
+    return max(people_with_relations) if people_with_relations else None
+
+def max_age_with_friends(group):
+    """Returns the maximum age of people in the group that have at least one friend."""
+    people_with_friends = [
+        person['age'] for person in group
+        if any(connection['connection'] == 'friend' for connection in person['connections'])
+    ]
+    return max(people_with_friends) if people_with_friends else None
+
+max_age_result = max_age(my_group)
+average_relations_result = average_relations(my_group)
+max_age_with_relations_result = max_age_with_relations(my_group)
+max_age_with_friends_result = max_age_with_friends(my_group)
+
+print(max_age_result)
+print(average_relations_result)
+print(max_age_with_relations_result)
+print(max_age_with_friends_result)
