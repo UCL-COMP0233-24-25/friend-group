8b31b81c9818d701fb208dc0f0cae685a65a8c3d
Rebecca Phelan <rebecca.phelan.21@ucl.ac.uk>
2024-10-14 14:44:43 +0100

    COmpleted task 7: the maximum age of people in the group the average (mean) number of relations among members of the group the maximum age of people in the group that have at least one relation [more advanced] the maximum age of people in the group that have at least one friend

--- group.py
+++ group.py
@@ -32,6 +32,11 @@ my_group = {
             "John":"cousin",
             "Zalika":"landlord",
         }
+    },
+    "Bill":{
+        "age":35,
+        "job":"chef",
+        "relationships":{}
     }
 }
 # print(my_group)
@@ -41,7 +46,7 @@ my_group = {
 
 # Print max age
 def get_max_age(dict):
-    '''Input a dictionary {"name" :{"age":_}}
+    '''Input a dictionary {"name" :{"age":_}, ...}
     Return: max age of person in group and their name'''
     max_age=0
     for name in dict:
@@ -51,7 +56,53 @@ def get_max_age(dict):
             oldest_person = name
     return max_age, oldest_person
 
-print(get_max_age(my_group))
+print(f'Max age of person in group and their name: {get_max_age(my_group)}.')
 
 
-# 
+# Average number of relations
+
+def get_average_number_relations(dict):
+    '''Input: dictionary {"name" :{"relationships":{"relation_name":"relaiton",...},...},...}
+    Output: mean number of relationships per person in group'''
+    number_relations=0
+    number_poeple =0
+    for name in dict:
+        number_relations += len(dict[name]["relationships"])
+        number_poeple +=1
+    return number_relations/number_poeple
+
+print(f"Average number of relationships per person in group {get_average_number_relations(my_group)}.")
+
+
+# Make age of people in group that have at least one relation
+
+def max_age_of_social_people(dict):
+    '''Input a dictionary {"name" :{"age":_}, ...}
+    Return: max age of person in group and their name, if they have at least one person in relationships'''
+    max_age=0
+    for name in dict:
+        age = dict[name]["age"]
+        if len(dict[name]["relationships"])>0:
+            if age > max_age:
+                max_age = age
+                oldest_person = name
+    return max_age, oldest_person
+
+print(f'Max age of person - with at least one relationship - in group and their name: {max_age_of_social_people(my_group)}.')
+
+
+# Make age of people in group that have at least one friend
+
+def max_age_of_friended_people(dict):
+    '''Input a dictionary {"name" :{"age":_}, ...}
+    Return: max age of person in group and their name, if they have at least one person in relationships'''
+    max_age=0
+    for name in dict:
+        age = dict[name]["age"]
+        if 'friend' in dict[name]["relationships"].values():
+            if age > max_age:
+                max_age = age
+                oldest_person = name
+    return max_age, oldest_person
+
+print(f'Max age of person - with at least one friend - in group and their name: {max_age_of_friended_people(my_group)}.')
