85a811939cf4657deed1cbd40e49071725299956
Victor Baycroft <victor.baycroft.24@ucl.ac.uk>
2024-10-14 10:38:28 +0100

    Creating a group of friends

--- group.py
+++ group.py
@@ -2,4 +2,13 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {"Jill":
+            {"Job":"Biologist", "Age":"26", "Relations":[("Zalika","Friend"),("John","partner")]},
+            "Zalika":
+            {"Job":"Artist", "Age":"28", "Relations":[("Jill","Friend")]},    
+            "John":
+            {"Job":"Writer", "Age":"27", "Relations":[("Zalika","Partner")]},
+            "Nash":
+            {"Job":"Chef", "Age":"34", "Relations":[("John","Cousin"),("Zalika","Landlord")]}}
+ 
+print(my_group["Jill"]["Relations"][0])
