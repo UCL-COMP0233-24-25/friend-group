f1f18cd2450615a9976eb8c697f316115646ab25
cansuoktem <cansu.oktem.21@ucl.ac.uk>
2024-10-14 14:20:09 +0100

    Adding new function for age and relations

--- group.py
+++ group.py
@@ -3,7 +3,7 @@
 # Your code to go here...
 
 my_group = {"Jill":
-            {"Job":"Biologist", "Age":"26", "Relations":[("Zalika","Friend"),("John","partner")]},
+            {"Job":"Biologist", "Age":"26", "Relations":[("Zalika","Friend"),("John","Partner")]},
             "Zalika":
             {"Job":"Artist", "Age":"28", "Relations":[("Jill","Friend")]},    
             "John":
@@ -11,4 +11,23 @@ my_group = {"Jill":
             "Nash":
             {"Job":"Chef", "Age":"34", "Relations":[("John","Cousin"),("Zalika","Landlord")]}}
  
-print(my_group["Jill"]["Relations"][0])
+
+def age_relation_info(group=my_group):
+    age_vec = []
+    age_relat_vec = []
+    age_friend_vec = []
+    relation_vec = []
+    for key in group:
+        age_vec.append(int(group[key]["Age"]))
+        if group[key]["Relations"]:
+            relation_vec.append(len(group[key]["Relations"]))
+            age_relat_vec.append(int(group[key]["Age"]))
+            for relation in group[key]["Relations"]:
+                if "Friend" in relation:
+                    age_friend_vec.append(int(group[key]["Age"]))
+
+    return(
+        f"Max Age: {max(age_vec)};  Average Relations: {sum(relation_vec)/len(group)};  Max Age with >=1 Relation: {max(age_relat_vec)};  Max Age with >=1 Friend: {max(age_friend_vec)}"
+    )
+
+age_relation_info()
