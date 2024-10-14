fd9352dbdee943a7f7533d487c4c8739bd4b74ee
PX-G <ucappg1@ucl.ac.uk>
2024-10-14 18:01:02 +0800

    Update group.py

--- group.py
+++ group.py
@@ -2,4 +2,7 @@
 
 # Your code to go here...
 
-my_group =
+my_group = [{ "name": "Jill", "age": 26, "job": biologist,"friend":"Zalika","partner":"John"}, 
+        {"name": "Zalika", "age": 28, "job": artist,"friend":"Jill"},
+        {"name": "John", "age": 27, "job": writer,"parter":"Jill"},
+           {"name": "Nash", "age": 34, "job": chef,"cousin":"John"}]
