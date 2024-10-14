111c6166c1c7330e50d93f7b710061cdc36a4cfe
Crows12138 <782990295@qq.com>
2024-10-14 11:22:41 +0100

    After formatting

--- group.py
+++ group.py
@@ -2,16 +2,39 @@
 
 # Your code to go here...
 
-my_group =[{"Yitian Xu":{"age":23,"job":None,"connection":[{"Zicong Peng":"friend"}]}},[{"Zicong Peng":{"age":22,"job":None,"connection":[{"Yitian Xu":"friend"}]}}]]
-example_group ={"Jill":{"age":26,"job":"biologist","connection":{"Zalika":"friend","John":"partner"}},
-                "Zalika":{"age":28,"job":"artist","connection":{"Jill":"friend"}},
-                "John":{"age":27,"job":"writer","connection":{"Jill":"partner"}},
-                "Nash":{"age":34,"job":"chef","connection":{"John":"cousin","Zalika":"landlord"}}}
+my_group = [
+    {"Yitian Xu": {"age": 23, "job": None, "connection": [{"Zicong Peng": "friend"}]}},
+    [
+        {
+            "Zicong Peng": {
+                "age": 22,
+                "job": None,
+                "connection": [{"Yitian Xu": "friend"}],
+            }
+        }
+    ],
+]
+example_group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "connection": {"Zalika": "friend", "John": "partner"},
+    },
+    "Zalika": {"age": 28, "job": "artist", "connection": {"Jill": "friend"}},
+    "John": {"age": 27, "job": "writer", "connection": {"Jill": "partner"}},
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "connection": {"John": "cousin", "Zalika": "landlord"},
+    },
+}
 
-#for person in my_group:
-    #if person[*arg["connection"]]=="none":
-        #my_group.remove(person)
+# for person in my_group:
+# if person[*arg["connection"]]=="none":
+# my_group.remove(person)
 
 print(example_group["Jill"]["connection"]["John"])
 for person in example_group:
-    print(f"{person} is {example_group[person]["age"]}, a {example_group[person]["job"]} ")
+    print(
+        f"{person} is {example_group[person]["age"]}, a {example_group[person]["job"]} "
+    )
