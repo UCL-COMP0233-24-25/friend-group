6e7148a0a810a65e3b74a5ac227c809750591e4b
Victor Baycroft <victor.baycroft.24@ucl.ac.uk>
2024-10-14 11:45:35 +0100

    problems1-3

--- group.py
+++ group.py
@@ -3,14 +3,40 @@
 # Creates a dictionary of dictionaries...
 
 my_group = {"Jill":
-            {"Job":"Biologist", "Age":"26", "Relations":[("Zalika","Friend"),("John","partner")]},
+            {"Job":"Biologist", "Age":26, "Relations":{"Zalika":"Friend", "John":"partner"}},
             "Zalika":
-            {"Job":"Artist", "Age":"28", "Relations":[("Jill","Friend")]},    
+            {"Job":"Artist", "Age":28, "Relations":{"Jill":"Friend"}},    
             "John":
-            {"Job":"Writer", "Age":"27", "Relations":[("Zalika","Partner")]},
+            {"Job":"Writer", "Age":27, "Relations":{"Zalika":"Partner"}},
             "Nash":
-            {"Job":"Chef", "Age":"34", "Relations":[("John","Cousin"),("Zalika","Landlord")]}}
- 
- # to access use my_group["Jill"].values() will give values or my_group["Jill"]["Relations"][0]
+            {"Job":"Chef", "Age":34, "Relations":{"John":"Cousin","Zalika":"Landlord"}}}
 
-print(my_group["Jill"]["Relations"][0])
+# to access use my_group["Jill"].values() will give values or my_group["Jill"]["Relations"][0]
+# ruff from command palette, ctrl shift p and format document will tidy up your code
+x=0
+i=0
+z=0
+p=0
+w=0
+for y in my_group:
+    i+=1
+    if my_group[y]["Age"] > x:
+        x = my_group[y]["Age"]
+
+    z+= len(my_group[y]["Relations"])
+
+    if len(my_group[y]["Relations"]):
+        if my_group[y]["Age"] > p:
+            p= my_group[y]["Age"]
+
+    if len(my_group[y]["Relations"]):
+        for key in my_group:
+            if my_group[y]["Relations"][key] == "Friend":
+                if my_group[y]["Age"] > w:
+                    w = my_group[y]["Age"]
+
+
+print(x) # max age
+print(z/i) # mean number of relations
+print(p) # max age of people with at least one relation
+
