b38abdabed62ab788749c97080c8571d21b1cc94
Victor Baycroft <victor.baycroft.24@ucl.ac.uk>
2024-10-14 11:03:46 +0100

    notes to help me

--- group.py
+++ group.py
@@ -1,6 +1,6 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
+# Creates a dictionary of dictionaries...
 
 my_group = {"Jill":
             {"Job":"Biologist", "Age":"26", "Relations":[("Zalika","Friend"),("John","partner")]},
@@ -11,4 +11,6 @@ my_group = {"Jill":
             "Nash":
             {"Job":"Chef", "Age":"34", "Relations":[("John","Cousin"),("Zalika","Landlord")]}}
  
+ # to access use my_group["Jill"].values() will give values or my_group["Jill"]["Relations"][0]
+
 print(my_group["Jill"]["Relations"][0])
