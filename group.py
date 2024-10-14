4d3a5099177204cb647bee2c182470a671c9a779
sanli <ucaphlu@ucl.ac.uk>
2024-10-14 10:29:19 +0100

    complete exer 1

--- group.py
+++ group.py
@@ -2,4 +2,24 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {}
+
+Jill = {'name':'Jill', 'age':'26', 'job':'biologist',
+            'connections':{'friend':['Zalika'],
+                           'partner':['John']}
+            }
+
+Zalika = {'name':'Zalika', 'age':'28', 'job':'biologist',
+            'connections':{'friend':['Jill']}
+            }
+
+Jhon = {'name':'Jhon', 'age':'27', 'job':'writer',
+            'connections':{'partner':['Jill']}
+            }
+
+Nash = {'name':'Nash', 'age':'34', 'job':'chef',
+            'connections':{'cousin':['Jhon'],
+                           'landlord':['Zalika']}
+            }
+
+my_group.append()
