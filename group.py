87e4163a21502b7c7584ccda3f3b04b171fe7b1b
Junru Wang <junru.wang.24@ucl.ac.uk>
2024-10-14 10:35:23 +0100

    Peter finished my group

--- group.py
+++ group.py
@@ -2,4 +2,36 @@
 
 # Your code to go here...
 
-my_group =
+my_group = [
+    {
+        "name": "Jill",
+        "job": "biologist",
+        "age": 26,
+        "friends": {"Zalika"},
+        "partner": "John",
+    },
+    {
+        "name": "Zalika",
+        "job": "artist",
+        "age": 28,
+        "friends": {"Jill"},
+        "landlord": "Nash",
+        "partner": None,
+    },
+    {
+        "name": "John",
+        "job": "writer",
+        "age": 27,
+        "friends": {},
+        "partner": "Jill",
+    },
+    {
+        "name": "Nash",
+        "job": "chef",
+        "age": 34,
+        "friends": {},
+        "cousin": {"John"},
+        "tenant": "Zalika",
+        "partner": "Jill",
+    },
+]
