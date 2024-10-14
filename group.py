5835d6789607de93d0357beccaa9a7ba1090cb87
Jennivine <45113203+Jennivine@users.noreply.github.com>
2024-10-14 22:00:41 +0100

    added comprehension expressions to get information about age and number of relations

--- group.py
+++ group.py
@@ -7,28 +7,41 @@ my_group = [
         "Name": "Jill",
         "Age": 26,
         "Job": "Biologist",
-        "Connections": {"Friends": ["Zalika"], 
-                        "Partner": "John"}
+        "Connections": {"Zalika": "friend", 
+                        "John": "partner"}
     },
     {
         "Name": "Zalika",
         "Age": 28,
         "Job": "Artist",
-        "Connections": {"Friends": ["Jill"], 
-                        "Landlord": "Nash"}
+        "Connections": {"Jill": "friend", 
+                        "Nash": "landlord"}
     },
     {
         "Name": "John",
         "Age": 27,
         "Job": "Writer",
-        "Connections": {"Partner": "Jill"}
+        "Connections": {"Jill": "partner"}
     },
     {
         "Name": "Nash",
         "Age": 34,
         "Job": "Chef",
-        "Connections": {"Cousins": ["John"], 
-                        "Tennants": ["Zalika"]}
+        "Connections": {"John": "cousin", 
+                        "Zalika": "tennant"}
     }
 ]
 
+import numpy as np
+
+max_age = max(person["Age"] for person in my_group)
+print(f"Maximum age: {max_age}")
+
+avg_nb_relations = np.mean([len(person["Connections"]) for person in my_group])
+print(f"Average number of relations: {avg_nb_relations}")
+
+max_age_with_at_least_one_relation = max(person["Age"] for person in my_group if len(person["Connections"]) >= 1)
+print(f"Maximum age of people who have at least one relation: {max_age_with_at_least_one_relation}")
+
+max_age_with_at_least_one_friend = max(person["Age"] for person in my_group if sum(relation=="friend" for relation in person["Connections"].values()) >= 1)
+print(f"Maximum age of people who have at least one friend {max_age_with_at_least_one_friend}")
