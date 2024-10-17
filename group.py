9f35c1f00598fc558a2b882e8fa95074c60ec368
Zhexuan <ucapzso@ucl.ac.uk>
2024-10-18 00:38:21 +0800

    Add error handling and change variable names

--- group.py
+++ group.py
@@ -10,14 +10,33 @@ my_group = {
 
 import numpy as np
 
-max_age = np.max([my_group[person]['age'] for person in my_group])
-print('the maximum age of people in the group is',max_age)
+try:
+    my_group
+except NameError:
+    print("Error: my_group is not defined!")
+else:
+    print("my_group exists and is ready to use.")
 
-average_rel = np.average([len(my_group[person]['relationship']) for person in my_group])
-print('the average (mean) number of relations among members of the group is',average_rel)
+try:
+    max_age = np.max([my_group[person]['age'] for person in my_group])
+    print('the maximum age of people in the group is',max_age)
+except Exception as e:
+    print('Error occurs when calculating.')
 
-max_con_age = np.max([my_group[person]['age'] for person in my_group if len(my_group[person]['relationship']) >= 1 ])
-print('the maximum age of people in the group that have at least one relation is',max_con_age)
+try:
+    average_relation = np.average([len(my_group[person]['relationship']) for person in my_group])
+    print('the average (mean) number of relations among members of the group is',average_relation)
+except Exception as e:
+    print('Error occurs when calculating.')
 
-max_fricon_age = np.max([my_group[person]['age'] for person in my_group if 'friend' in my_group[person]['relationship'] and len(my_group[person]['relationship']['friend']) >= 1 ])
-print('the maximum age of people in the group that have at least one friend is', max_fricon_age)
+try:
+    con_age1 = np.max([my_group[person]['age'] for person in my_group if len(my_group[person]['relationship']) >= 1 ])
+    print('the maximum age of people in the group that have at least one relation is',con_age1)
+except Exception as e:
+    print('Error occurs when calculating.')
+
+try:
+    con_age2 = np.max([my_group[person]['age'] for person in my_group if 'friend' in my_group[person]['relationship'] and len(my_group[person]['relationship']['friend']) >= 1 ])
+    print('the maximum age of people in the group that have at least one friend is', con_age2)  
+except Exception as e:
+    print('Error occurs when calculating.')
