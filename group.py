f602c743dccd6e3e0a47ad3d2acb6cee325d8aa1
AngelosZou <yutong.zou.24@ucl.ac.uk>
2024-10-19 15:12:28 +0100

    Add cousin relation to John, add 2-side relation to structure

--- group.py
+++ group.py
@@ -2,11 +2,28 @@
 import numpy as np
 
 #For the relationship of each person, it is like: the front person is the “relationship” of the behind person. 
+# A with relationship "rel": [B] means A is B's rel, while A with relation_from "rel": [B] means B is A's rel.
 my_group = {
-    "Jill":{"age":26,"job":"biologist","relationship":{"friend":["Zalika"],"partner":["John"]}},
-    "Zalika":{"age":28,"job":"artist","relationship":{"friend":["Jill"]}},
-    "John":{"age":27,"job":"writer","relationship":{"partner":["Jill"]}},
-    "Nash":{"age":34,"job":"chef","relationship":{"landlord":["Zalika"]}},
+    "Jill":{"age":26,
+            "job":"biologist",
+            "relationship":{"friend":["Zalika"],"partner":["John"]},
+            "relation_from":{"friend":["Zalika"],"partner":["John"]}
+            },
+    "Zalika":{"age":28,
+              "job":"artist",
+              "relationship":{"friend":["Jill"]},
+              "relation_from":{"friend":["Jill"], "landlord":["Nash"]}
+              },
+    "John":{"age":27,
+            "job":"writer",
+            "relationship":{"partner":["Jill"], "cousin": ["Nash"]},
+            "relation_from":{"partner":["Jill"], "cousin":["Nash"]}
+            },
+    "Nash":{"age":34,
+            "job":"chef",
+            "relationship":{"landlord":["Zalika"], "cousin":["John"]},
+            "relation_from":{"cousin": ["John"]}
+    }
 }
 
 
